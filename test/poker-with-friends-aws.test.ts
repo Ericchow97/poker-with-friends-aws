6c665b0f07df7f34a64b53c1fd1b38f1c1c53db7
Ericchow97 <36716562+Ericchow97@users.noreply.github.com>
2022-12-14 23:50:07 -0500

    Initial Commit using AWS CDK

---
+++ test/poker-with-friends-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PokerWithFriendsAws from '../lib/poker-with-friends-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/poker-with-friends-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PokerWithFriendsAws.PokerWithFriendsAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
