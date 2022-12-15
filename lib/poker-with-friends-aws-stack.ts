4a00bf32f8fa0e2fb4fbdbda30a83de23dea4ffd
Ericchow97 <36716562+Ericchow97@users.noreply.github.com>
2022-12-15 17:39:27 -0500

    Refactor code
    Separate out Lambda functions from Websocket
    Rewrite Websocket to handle more general functions

--- lib/poker-with-friends-aws-stack.ts
+++ lib/poker-with-friends-aws-stack.ts
@@ -1,12 +1,31 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import { HelloWorld } from './websocket-helpers/lambda/hello';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
+
 import { PWFApi } from './websocket-helpers/api-gateway/PWFApi';
 
 export class PokerWithFriendsAwsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    // new HelloWorld(this, 'hello-world');
-    new PWFApi(this, 'PWFApi')
+
+
+    //TODO: Update connect & disconnect functions
+    // create lambda functions
+    // connect function
+    const connectFn = new NodejsFunction(this, 'connectFn', {
+      entry: path.resolve(__dirname, "websocket-helpers", "lambda", 'hello.function.ts')
+    })
+
+    // disconnect function
+    const disconnectFn = new NodejsFunction(this, 'disconnectFn', {
+      entry: path.resolve(__dirname, "websocket-helpers", "lambda", 'hello.function.ts')
+    })
+
+    // create API Gateway
+    new PWFApi(this, 'PWFApi', {
+      connectFn,
+      disconnectFn
+    })
   }
 }
